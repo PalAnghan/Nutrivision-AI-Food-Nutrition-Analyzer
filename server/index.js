7857a9d05762514884ae2a167bf67c31f543b8a8
PalAnghan <palanghan750@gmail.com>
2025-12-31 17:13:31 +0530

    Fix CORS for Netlify frontend

--- server/index.js
+++ server/index.js
@@ -14,7 +14,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "*",
+    origin: [
+      "http://localhost:5173",
+      "https://nutrivision-ai-food-nutrition-analyze.netlify.app"
+    ],
     methods: ["GET", "POST", "DELETE"],
   })
 );
