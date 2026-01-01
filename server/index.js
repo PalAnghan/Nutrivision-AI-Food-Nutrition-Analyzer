9d129adca33f116257cba7e975fa6bebd13d465e
PalAnghan <palanghan750@gmail.com>
2026-01-01 12:07:46 +0530

    Fix server crash: add body parsers

--- server/index.js
+++ server/index.js
@@ -12,6 +12,9 @@ import healthRoutes from "./routes/healthRoutes.js";
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.use(cors({
   origin: [
